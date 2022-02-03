2412a13b72e8904c1d31cb4b2ccb865b678147ad
polinaLyubavina <polinalyub@tutanota.com>
2022-02-03 11:54:32 -0700

    add cmdline arguments --print, --pretty-print, --interp

--- cmdline.cpp
+++ cmdline.cpp
@@ -5,23 +5,42 @@
 #include "catch.h"
 
 
-// definition for use_arguments
+    // definition for use_arguments
+    std::string help = "--help"; 
+    std::string test = "--test"; 
+    std::string interp = "--interp";
+    std::string print = "--print";
+    std::string pretty_print = "--pretty-print";
+
+    std::string cmdline_out = "Arguments available for use are:\n --test\n --help\n --interp\n --print\n --pretty-print\n";
+
 
 void use_arguments(int argc, char **argv) {
 
     std::string argvalue1 = argv[1];
-    std::string help = "--help"; 
-    std::string test = "--test"; 
 
+    //begin command line output
     if(argc == 2) {
 
         if(argvalue1.compare(help) == 0) {
-            std::cout << "Arguments available to you are: --test, --help\n";
+            std::cout << cmdline_out;
         }
 
         else if(argvalue1.compare(test) == 0) {
             // std::cout << "Tests passed\n";
             Catch::Session().run(1, argv);
+        }
+
+        else if(argvalue1.compare(interp) == 0) {
+
+        }
+
+        else if(argvalue1.compare(print) == 0) {
+
+        }
+
+        else if(argvalue1.compare(pretty_print) == 0) {
+            
         }
 
         else {
