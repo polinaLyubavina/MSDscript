2153a6855afb16d978bae6cde122ae46d6b56eed
polinaLyubavina <polinalyub@tutanota.com>
2022-02-14 14:12:42 -0700

    remove parse_add() and parse_mult, add parse()

--- parse.h
+++ parse.h
@@ -4,26 +4,20 @@
 #include <string.h>
 #include <sstream>
 
-class Parse : public Expr {
 
-    public:
+Expr* parse(std::istream &input);
 
-        Expr* parse_expr(std::istream& input);
+Expr* parse_expr(std::istream &input);
 
-        Expr* parse_num(std::istream& input);
-        Expr* parse_var(std::istream& input);
-        Expr* parse_add(std::istream& input);
-        Expr* parse_mult(std::istream& input);
+Expr* parse_num(std::istream &input);
+Expr* parse_var(std::istream &input);
 
-        Expr* parse_let(std::istream& input);
+Expr* parse_let(std::istream &input);
 
-        //what are these??
-        Expr* parse_addend(std::istream& input);
-        Expr* parse_multicand(std::istream& input);
+Expr* parse_addend(std::istream &input);
+Expr* parse_multicand(std::istream &input);
 
-        //helpers
-        static void skip_whitespace(std::istream& input); 
-        static void consume(std::istream& input, int expect);  
-        Expr* parse_keyword(std::istream& input); 
+Expr* parse_keyword(std::istream &input); 
 
-};
+static void skip_whitespace(std::istream &input); 
+static void consume(std::istream &input, int expect);  
